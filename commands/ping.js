a03f0aaed4da0e20ec8fc4c02a465ab8dc13d958
Aidan <datbluemushroom@gmail.com>
2023-04-15 21:25:13 -0400

    🐛 Changed Ping

--- commands/ping.js
+++ commands/ping.js
@@ -5,12 +5,17 @@ module.exports = {
     .setName("ping")
     .setDescription("Replies with actual bot ping! by Fluid#2654"),
   async execute(interaction) {
-    const message = await interaction.channel.send("Pinging... :ping_pong:");
-		const ping = interaction.client.ws.ping;
-
-		message.edit([
-			`:ping_pong: Client ping: ${message.createdTimestamp - interaction.createdTimestamp}ms`,
-			`:robot: WebSocket ping: ${ping}ms`
-		].join("\n"));
+    await ping(interaction)
   },
 };
+
+async function ping(interaction)
+{
+  const message = await interaction.reply("Pinging... :ping_pong:");
+  const ping = interaction.client.ws.ping;
+
+  message.edit([
+    `:ping_pong: Client ping: ${message.createdTimestamp - interaction.createdTimestamp}ms`,
+    `:robot: WebSocket ping: ${ping}ms`
+  ].join("\n"));
+}

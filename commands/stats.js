021f245600e373c2f247b1990147a6f9b5d166ef
Joshua Hendrix <thejoshuahendrix@gmail.com>
2023-04-16 11:10:59 -0400

    🚀 Server Stats

---
+++ commands/stats.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('stats')
+		.setDescription('Shows server stats!'),
+	async execute(interaction) {
+		await generateStats(interaction);
+	},
+};
+
+
+async function generateStats(interaction)
+{
+  const message = await interaction.reply("Generating... :gear:");
+
+  // get server stats
+    const guild = interaction.guild;
+    const roles = guild.roles.cache;
+    const emojisCount = emojis.size;
+    const rolesCount = roles.size;
+
+  message.edit([
+    `:gear: Server stats:`,
+    `:robot: Server name: ${guild.name}`,
+    `:robot: Server ID: ${guild.id}`,
+    `:robot: Server owner: ${guild.owner}`,
+    `:robot: Server region: ${guild.region}`,
+    `:robot: Server member count: ${guild.memberCount}`,
+    `:robot: Server roles count: ${rolesCount}`,
+    `:robot: Server emojis count: ${emojisCount}`
+  ].join("\n"));
+}
